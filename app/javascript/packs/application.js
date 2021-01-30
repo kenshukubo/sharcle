5d4d3028867dc443efe5f42a8eab80129d31af33
kenshukubo <kensyu124logic@yahoo.co.jp>
2021-01-31 01:14:52 +0900

    fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,10 +2,16 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+require("@rails/ujs").start()
+require("@rails/activestorage").start()
+require("channels")
 
-import Rails from "@rails/ujs"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+require("jquery")
+require("public")
 
-Rails.start()
-ActiveStorage.start()
+// import Rails from "@rails/ujs"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
+
+// Rails.start()
+// ActiveStorage.start()
