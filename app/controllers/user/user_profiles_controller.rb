50f80cb138bc157b623b04c9985bf1b3ff5b642d
kenshukubo <kensyu124logic@yahoo.co.jp>
2021-02-03 10:33:28 +0900

    fix

--- app/controllers/user/user_profiles_controller.rb
+++ app/controllers/user/user_profiles_controller.rb
@@ -1,22 +1,17 @@
 class User::UserProfilesController < ApplicationController
   before_action :set_user_profile, only: [:show, :edit, :update]
-  # GET /user_profiles/1/edit
+
   def edit
     @user = current_user
-    # @user_profile.update_zoom_profile!
   end
 
-  # PATCH/PUT /user_profiles/1
-  # PATCH/PUT /user_profiles/1.json
   def update
-    respond_to do |format|
-      if @user_profile.update(user_profile_params)
-        format.html { redirect_to edit_user_profile_path, notice: 'プロフィールを更新しました' }
-        format.json { render :show, status: :ok, location: @user_profile }
-      else
-        format.html { render :edit }
-        format.json { render json: @user_profile.errors, status: :unprocessable_entity }
-      end
+    if @user_profile.update(user_profile_params)
+      flash[:notice] = "プロフィールを更新しました"
+      redirect_to edit_user_profile_path
+    else
+      flash[:notice] = "プロフィールに失敗しました"
+      redirect_to edit_user_profile_path
     end
   end
 
@@ -29,6 +24,6 @@ class User::UserProfilesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_profile_params
-      params.require(:user).permit(user_profile_attributes:[:id, :name])
+      params.require(:user_profile).permit(:name)
     end
 end
